41bdc83f40fd8ded645769c9b46f9e4c173c1dcf
dnwakshe <[wakshe2@gmail.com]>
2021-05-18 23:06:54 +0530

    Added webview to project

---
+++ lib/view/screens/twittercard.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+import 'webviewer.dart';
+
+class twittercard extends StatefulWidget {
+  @override
+  _twittercardState createState() => _twittercardState();
+}
+
+class _twittercardState extends State<twittercard> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(      
+      body:
+        Center( 
+          child: Container(
+              height: 110, 
+              child: Card(
+                      child: Column(
+                         children: [
+                    Container(                                              
+                          child: IconButton(
+                            icon: Image.asset('images/twitter_icon.png',),            
+                            iconSize: 60,                                                   
+                              onPressed: (){   
+                                Navigator.of(context)
+                                  .push(MaterialPageRoute(builder:(_)=>Web()));                          
+                                }
+                         ),
+                    ),
+                            Padding(
+                              padding: const EdgeInsets.only(bottom: 8.0),
+                              child: Text('Go to Web'),
+                            ),
+                              ],
+                            ),   
+              ),
+              
+            ),
+            
+        ),
+    );
+  }
+}
---
+++ lib/view/screens/webviewer.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class Web extends StatelessWidget {
+  final String url;
+  Web({this.url});
+  @override
+  Widget build(BuildContext context) {
+   return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Welcome to flutter',
+      home: Scaffold(        
+        body: WebView(
+          initialUrl: 'https://en.wikipedia.org',
+          javascriptMode: JavascriptMode.unrestricted,
+        ),
+      ),
+    );
+  }
+}
